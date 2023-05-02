1afad0c7cb5acbca4a442a12eab4ecdaa8501e72
root <root@sandbox-hdp.hortonworks.com>
2023-05-02 15:19:22 +0000

    Added CREATE and DROP DATABASE

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+echo
+
+# drop the databases if exist
+psql -U postgres -c 'DROP DATABASE IF EXISTS project;'
+
+# create a database project
+psql -U postgres -c 'CREATE DATABASE project;'
+
