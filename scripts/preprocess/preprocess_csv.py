1c6ac730a8169cece999ee720404f899e6af34ad
root <root@sandbox-hdp.hortonworks.com>
2023-05-05 10:05:08 +0000

    sql finished

--- scripts/preprocess/preprocess_csv.py
+++ scripts/preprocess/preprocess_csv.py
@@ -7,8 +7,11 @@ movies_path = 'data/movie_data.csv'
 movies_path_pd = 'data/movie_data_pd.csv'
 movies = pd.read_csv(movies_path, lineterminator='\n', index_col='_id')
 
-ratings_path = 'data/movie_data.csv'
-#ratings = pd.read_csv(ratings_path, lineterminator='\n')
+
+ratings_path = 'data/ratings_export.csv'
+ratings_path_pd = 'data/ratings_export_pd.csv'
+ratings = pd.read_csv(ratings_path, lineterminator='\n', )
+
 
 def to_psql_array(x):
 	if type(x) == str:
@@ -19,19 +22,18 @@ def to_psql_array(x):
 		x = '{}'
 	return x
 
+
 movies.genres = movies.genres.apply(lambda x: to_psql_array(x))
 
 # remove carriage return symbols
 movies = movies.replace({r'\r': ''}, regex=True)
 
-# preprocess ratings table
-#ratings = ratings.replace({'movie_id': {'null': 'null_'} })
 
 # fix id of the film "(NULL)"
 
-print("\n================================ np.nan")
-movies_nullid = movies[movies['movie_id']==np.nan]
-display(movies_nullid[['movie_id', 'imdb_id']])
+#print("\n================================ np.nan")
+#movies_nullid = movies[movies['movie_id']==np.nan]
+#display(movies_nullid[['movie_id', 'imdb_id']])
 
 # movies = movies.replace({'movie_id': {np.nan: 'null_id'}}) 
 #movies.loc[movies['imdb_id']=='tt3790100'] = \
@@ -45,19 +47,28 @@ movies.at['5fd08f22b89a841f5b234f50', 'movie_id'] = 'null_id'
 # replace np.nan with null
 movies = movies.replace({np.nan: ''})
 
+
+# fix ratings table
+ratings = ratings.replace({'movie_id': {np.nan: 'null_id', 'nan': 'nan_id'} })
+
+ratings = ratings.drop(ratings[ratings.user_id == 'jacksonmaines'].index)
+
+#left_join = ratings.merge(movies, on='movie_id', how='left', indicator=True)
+ratings = ratings.drop(ratings[~ratings.movie_id.isin(movies.movie_id)].index)
+
 # save new csv
 movies.to_csv(movies_path_pd, index=True)
-#ratings.to_csv(ratings_path, index=True)
+ratings.to_csv(ratings_path_pd, index=False)
 
 
-movies = pd.read_csv(movies_path_pd, lineterminator='\n')
+#movies = pd.read_csv(movies_path_pd, lineterminator='\n')
 
-print("\n================================ null_id")
-movies_nullid = movies[movies['movie_id']=='null_id']
-display(movies_nullid[['movie_id', 'imdb_id']])
+#print("\n================================ null_id")
+#movies_nullid = movies[movies['movie_id']=='null_id']
+#display(movies_nullid[['movie_id', 'imdb_id']])
 
-nullrow = movies[movies['imdb_id']=='tt3572736'].iloc[0]
-display(nullrow)
+#nullrow = movies[movies['imdb_id']=='tt3572736'].iloc[0]
+#display(nullrow)
 
-print(nullrow.movie_id)
-print(type(nullrow.movie_id))
+#print(nullrow.movie_id)
+#print(type(nullrow.movie_id))
--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -3,87 +3,11 @@
 # echo a blank line for the sake of output readability
 echo
 
+# run the sql script to build a relational database
+psql -U postgres -d project -f stage1/db.sql
+
+# some operation on the resulted DB
 echo "
-
--- create a database project
-DROP DATABASE IF EXISTS project;
-CREATE DATABASE project;
-
-
---switch to the database project
 \c project;
-
-
--- add tables 
-
-CREATE TABLE users (
-	_id TEXT NOT NULL,
-	display_name TEXT,
-	num_ratings_pages INTEGER,
-	num_reviews INTEGER,
-	username TEXT NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE movies (
-	_id TEXT NOT NULL,
-	genres TEXT [],
-	_image_url TEXT,
-	imdb_id TEXT,
-	_imdb_link TEXT,
-	movie_id TEXT NOT NULL PRIMARY KEY,
-	movie_title TEXT,
-	original_language TEXT,
-	overview TEXT,
-	popularity REAL,
-	temp_production_countries TEXT,
-	release_date DATE,
-	runtime REAL,
-	temp_spoken_languages TEXT,
-	_tmdb_id TEXT,
-	_tmdb_link TEXT,
-	vote_average REAL,
-	vote_count REAL,
-	year_released REAL
-);
--- runtime: convert 0 and null to NULL
-
-CREATE TABLE ratings (
-	id TEXT NOT NULL PRIMARY KEY,
-	movie_id TEXT NOT NULL,
-	rating_val INTEGER NOT NULL,
-	user_id TEXT NOT NULL
-);
-
-
--- load data from csv files
-SET datestyle TO iso, ymd;
-
-\COPY users FROM 'data/users_export.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER, NULL '');
-\COPY movies FROM 'data/movie_data_pd.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER, NULL '');
-\COPY ratings FROM 'data/ratings_export.csv' WITH (FORMAT CSV, DELIMITER ',', HEADER, NULL '');
-
-
--- add constraints
-
-ALTER TABLE ratings
-ADD CONSTRAINT fk_user_username_userid
-FOREIGN KEY (user_id)
-REFERENCES users(username);
-
-ALTER TABLE ratings
-ADD CONSTRAINT fk_movie_movieid_movieid
-FOREIGN KEY (movie_id)
-REFERENCES movies(movie_id);
-
-
--- SELECT * FROM movies LIMIT 10;
-
-
-
+SELECT * FROM ratings LIMIT 3;
 " | psql -U postgres
-
-
-
-
-
-
