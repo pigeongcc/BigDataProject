e8c7133a203f7ca65440d168d597e4819b3e4371
pigeongcc <gulyachannel@gmail.com>
2023-05-07 14:05:14 +0000

    added code for psql config edit & JDBC driver installation

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -12,3 +12,16 @@ echo "
 \c project;
 SELECT * FROM ratings LIMIT 3;
 " | psql -U postgres
+
+# edit PostgreSQL config
+# sed -i '1s/^/<local all all trust> /' /var/lib/pgsql/data/pg_hba.conf
+LINE='local all all trust'
+FILE='/var/lib/pgsql/data/pg_hba.conf'
+grep -xqF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# restart PostgreSQL server
+systemctl restart postgresql
+
+# download (if not exists) and install the JDBC driver
+wget -nc https://jdbc.postgresql.org/download/postgresql-42.6.0.jar --no-check-certificate
+cp  postgresql-42.6.0.jar /usr/hdp/current/sqoop-client/lib/
