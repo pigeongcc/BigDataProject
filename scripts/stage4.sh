49e87d2f71bc97aa5c091a5653da976c5e6741cc
pigeongcc <gulyachannel@gmail.com>
2023-05-11 12:13:23 +0000

    added background run for streamlit

--- scripts/stage4.sh
+++ scripts/stage4.sh
@@ -2,4 +2,4 @@
 
 # run streamlit app
 # streamlit run scripts/stage4/streamlit_sample.py --server.port 60000
-streamlit run scripts/stage4/streamlit_app.py --server.port 60000
+streamlit run scripts/stage4/streamlit_app.py --server.port 60000 &
